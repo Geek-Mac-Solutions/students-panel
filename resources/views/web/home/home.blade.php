6a2880a1d3563457e3f2150d3cf1858bb3b79c5f
nadeechathu <nadishanchathuranga21@gmail.com>
2024-06-14 22:50:24 +0530

    table section done

--- resources/views/web/home/home.blade.php
+++ resources/views/web/home/home.blade.php
@@ -12,15 +12,29 @@
          </h1>
       </div>
       <div class="col-lg-3 col-sm-3">
-         <div class="row  justify-content-end float-lg-end">
+         <div class="row  justify-content-center">
             <div class="col-12 pe-lg-5 pt-lg-0 pt-3">
-               <div class="form-check form-switch cus-switch  ps-lg-5 ms-lg-3 ps-sm-0">
+               <div class="form-check form-switch cus-switch ">
                   <div class="row align-items-end">
-                     <div class="col-3">   <label class="form-check-label font-14 fw-bolder text-uppercase text-purple" for="flexSwitchCheckDefault">BASIC</label>
+                     <div class="col-4"> <label class="form-check-label font-12 fw-bolder text-uppercase text-purple" for="flexSwitchCheckDefault">BASIC</label>
                      </div>
                      <div class="col-lg-4 col-sm-5  col-3">   <input class="form-check-input font-14  w-100 pt-3 pb-2 text-uppercase text-purple ms-0" type="checkbox" id="flexSwitchCheckDefault">
                      </div>
-                     <div class="col-3">   <label class="form-check-label font-14 fw-bolder text-uppercase text-purple" for="flexSwitchCheckDefault">ADVANCED</label>
+                     <div class="col-4">   <label class="form-check-label font-12 fw-bolder text-uppercase text-purple" for="flexSwitchCheckDefault">ADVANCED</label>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="row  justify-content-center">
+            <div class="col-12   pt-lg-0 pt-3">
+               <div class="form-check form-switch ">
+                  <div class="row align-items-end">
+                     <div class="col-4">   <label class="form-check-label font-12 fw-bolder text-uppercase text-purple" for="flexSwitchCheckDefault">Grade 06</label>
+                     </div>
+                     <div class="col-lg-4 col-sm-5  col-3">   <input class="form-check-input font-14  w-100 pt-3 pb-2 text-uppercase text-purple ms-0" type="checkbox" id=" ">
+                     </div>
+                     <div class="col-4">   <label class="form-check-label font-12 fw-bolder text-uppercase text-purple" for="flexSwitchCheckDefault">Grade 11</label>
                      </div>
                   </div>
                </div>
@@ -45,6 +59,7 @@
                   </div>
                </div>
             </a>
+            
          </div>
          <div class="col-lg-3 col-sm-5  col-10 pt-lg-0 pt-sm-0 pt-3">
             <a href="{{ route('web.video.view') }}"  class="hvr-shrink w-100">
--- resources/views/web/videos/subject_list.blade.php
+++ resources/views/web/videos/subject_list.blade.php
@@ -26,20 +26,47 @@
 <div class="container-fluid py-lg-5 py-3 px-lg-5 ">
    <div class="row middle-hight">
        <div class="col-12">
-        <table class="display" id="ports-table">
-            <thead>
-                <tr>
-                    <th>Dev_id</th>
-                    <th>Portnumber</th>
-                    <th>Descriiption</th>
-                    <th>Speed</th>
-                    <th>Created At</th>
-                    <th>Updated At</th>
-                </tr>
-            </thead>
-        </table>
-       </div>
 
+
+   
+
+
+
+          <div class="table-responsive">
+            <table id="example" class="table table-striped table-hover table-bordered">
+               <thead class="text-white gradient-background text-uppercase fw-light font-15">
+                  <tr>
+                     <th>MONTH</th>
+                     <th>CLASS  Date</th>
+                     <th>GRADE</th>
+                     <th>TOPIC</th>
+                     <th>VIDEO</th>
+                  </tr>
+               </thead>
+               <tbody class="font-14 align-items-center">
+                  <tr>
+                     <td>JANUARY</td>
+                     <td>01.01.2024</td>
+                     <td>GRADE 6</td>
+                     <td >Grade 6 - Unit 08 - Electricity for a Comfortable
+                        Life - Part 01 (English Medium) - 2023.07.19
+                     </td>
+                     <td>
+                        <a class="w-100 text-uppercase font-10 text-white rounded-pill py-1 px-3 bg-success fw-bolder align-items-center text-white hvr-shrink   "  href="https://www.youtube.com/watch?v=tQTfF4FRA2k&list=PLMrrM6QnSnHwCdEQ7IAgFe0WDn21Dua5T" target="_blank">
+                        View More
+                        </a>
+                     </td>
+                  </tr>
+               </tbody>
+            </table>
+         </div>
+
+
+
+
+
+
+    </div>
 
 
 
